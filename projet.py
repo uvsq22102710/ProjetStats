d755e73af4aaac9828ba0cdf0d592daf0f3dfbd9
frederique ngoma <frederique.ngoma-moutsinga@ens.uvsq.fr>
2022-04-19 11:08:57 +0200

    fred

--- projet.py
+++ projet.py
@@ -1,12 +1,26 @@
 import random as rd
 
+#nb= 8
+
 
 def cree_fichier_alea(nb, nomfichier):
-    nomfichier = open("fichier de texte", "a","w")
+    nomfichier = open("fichier de texte","w")
     for i in range(2):
-        temp = rd.uniform(0, 500)
+        temp = str(rd.uniform(0, 500))
         nomfichier.write(temp, " ")
     nomfichier.write("\n")
+    nomfichier.close()
 
 
 def lit_fichier(nomfic):
+    liste1 =[]
+    liste2 =[]
+    nomfichier = open("fichier de texte", "r")
+    line = nomfichier.readline()
+    val = line.split()
+    for i  in line:
+        for i in val:
+            liste1.append(val[0])
+
+
+
